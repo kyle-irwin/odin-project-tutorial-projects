88951e707e33513da34f1894861d7022fd3c60d2
Kyle Irwin <kyleirwin00@gmail.com>
2021-11-07 19:15:00 -0600

    work

--- rockpaperscissors/script.js
+++ rockpaperscissors/script.js
@@ -4,24 +4,19 @@ let paper = 'paper';
 let scissors = 'scissors';
 
 const rockButton = document.createElement('button');
-rockButton.innerText = 'rock';
-const paperButton = document.createElement('button');
-paperButton.innerText = 'paper';
-const scissorsButton = document.createElement('button');
-scissorsButton.innerText = 'scissors';
 
 
-let computerSelection; 
+let computerSelection;
 let userSelection;
 let computerScore = 0;
 let userScore = 0;
-let roundResult
+let roundResult;
 let printRoundResult = document.createElement('p');
 
-function getComputer () {
+function getComputer() {
     /*this function gets the computers random choice 
     and sets it to the var computerSelection*/
-    let rng = Math.floor(Math.random()*1000);
+    let rng = Math.floor(Math.random() * 1000);
     if (rng <= 333) {
         computerSelection = rock;
     }
@@ -33,42 +28,44 @@ function getComputer () {
     }
     return computerSelection;
 }
-function getUser () { 
+function getUser() {
     gameScreen.appendChild(rockButton)
     gameScreen.appendChild(paperButton)
     gameScreen.appendChild(scissorsButton)
-    userSelection = rock;
-    /*rockButton.addEventListener('click', () => { I DO NOT KNOW HOW TO WAIT FOR THE EVENT AND THEN MAKE THE USERSELECTION VARIABLE THAT EVENT
+
+    rockButton.addEventListener('click', () => {
         userSelection = rock;
         return userSelection;
     })
     paperButton.addEventListener('click', () => {
         userSelection = paper;
+        return userSelection;
     })
     scissorsButton.addEventListener('click', () => {
         userSelection = scissors;
-    })*/
+        return userSelection;
+    })
 }
-function playRound () {
+function playRound() {
     if (userSelection === rock && computerSelection === rock) {
         roundResult = 'Tie';
     }
-    else if (userSelection === rock && computerSelection === paper ) {
+    else if (userSelection === rock && computerSelection === paper) {
         roundResult = 'You Lose';
     }
-    else if (userSelection === rock  && computerSelection === scissors ) {
+    else if (userSelection === rock && computerSelection === scissors) {
         roundResult = "You Win";
     }
-    else if (userSelection === paper  && computerSelection === rock  ) {
+    else if (userSelection === paper && computerSelection === rock) {
         roundResult = "You Win";
     }
     else if (userSelection === paper && computerSelection === paper) {
         roundResult = 'Tie';
-    }    
+    }
     else if (userSelection === paper && computerSelection === scissors) {
         roundResult = 'You Lose';
     }
-    else if (userSelection === scissors && computerSelection === rock ) {
+    else if (userSelection === scissors && computerSelection === rock) {
         roundResult = 'You Lose';
 
     }
@@ -82,22 +79,22 @@ function playRound () {
     printRoundResult.textContent = roundResult;
     gameScreen.appendChild(printRoundResult)
 }
-function printSelections () {
+function printSelections() {
     gameScreen.removeChild(rockButton)
     gameScreen.removeChild(paperButton)
     gameScreen.removeChild(scissorsButton)
-    
+
     let printUser = document.createElement('p');
     printUser.textContent = 'Your selection is ' + userSelection;
     gameScreen.appendChild(printUser)
-    
+
     let printComputer = document.createElement('p');
     printComputer.textContent = 'Your opponent\'s selection is ' + computerSelection;
     gameScreen.appendChild(printComputer)
 }
 
 
-function game () {
+function game() {
     getUser()
     getComputer()
     printSelections()
@@ -105,9 +102,9 @@ function game () {
 
 }
 
-
 const startGame = document.getElementById('startGame');
 startGame.addEventListener('click', () => {
     gameScreen.removeChild(startGame);
+    document.body.appendChild(rockButton);
     game();
 });
--- rockpaperscissors/script2.js
+++ rockpaperscissors/script2.js
@@ -1,98 +1,104 @@
 /*let gameScreen = document.getElementById('gameScreen')
 
-let computerSelection; 
+let computerSelection;
 function computerPlay () {
-    /*this function gets the computers random choice 
+    /*this function gets the computers random choice
     and sets it to the var computerSelection*/ /*
-    let rng = Math.floor(Math.random()*1000);
-    if (rng <= 333) {
-        computerSelection = 'rock';
-    }
-    else if (rng >= 334 && rng <= 667) {
-        computerSelection = 'paper';
-    }
-    else if (rng >= 668) {
-        computerSelection = 'scissors';
-    }
-    return computerSelection;
+let rng = Math.floor(Math.random()*1000);
+if (rng <= 333) {
+computerSelection = 'rock';
+}
+else if (rng >= 334 && rng <= 667) {
+computerSelection = 'paper';
+}
+else if (rng >= 668) {
+computerSelection = 'scissors';
+}
+return computerSelection;
 }
 
 let userSelection;
 let roundResult;
 let rock = document.createElement('button');
-    rock.innerText = 'rock';
-    let paper = document.createElement('button');
-    paper.innerText = 'paper';
-    let scissors = document.createElement('button');
-    scissors.innerText = 'scissors';
+rock.innerText = 'rock';
+let paper = document.createElement('button');
+paper.innerText = 'paper';
+let scissors = document.createElement('button');
+scissors.innerText = 'scissors';
 
 function playRound () {
-    computerPlay() 
-    if (rock) {
-        console.log('rock')
-        userSelection = rock;
-    }
-    else if (paper) {
-        console.log('paper')
-        userSelection = paper;
-    }
-    else {
-        console.log('scissors')
-        userSelection = scissors;
-    }
+computerPlay()
+if (rock) {
+console.log('rock')
+userSelection = rock;
+}
+else if (paper) {
+console.log('paper')
+userSelection = paper;
+}
+else {
+console.log('scissors')
+userSelection = scissors;
+}
 
 
-    console.log(computerSelection)
+console.log(computerSelection)
 
 
-    /*if (userSelection === 'rock' && computerSelection === 'rock') {
-        roundResult = 'Tie';
-        return roundResult; 
+/*if (userSelection === 'rock' && computerSelection === 'rock') {
+roundResult = 'Tie';
+return roundResult;
 
-    }
-    else if (userSelection === 'rock' && computerSelection === 'paper') {
-        roundResult = 'You lose. Paper covers rock';
-        return roundResult; 
+}
+else if (userSelection === 'rock' && computerSelection === 'paper') {
+roundResult = 'You lose. Paper covers rock';
+return roundResult;
 
-    }
-    else if (userSelection === 'rock' && computerSelection === 'scissors') {
-        roundResult = 'You win. Rock beats scissors';
-        return roundResult; 
+}
+else if (userSelection === 'rock' && computerSelection === 'scissors') {
+roundResult = 'You win. Rock beats scissors';
+return roundResult;
 
-    }
-    else if (userSelection === 'paper' && computerSelection === 'scissors') {
-        roundResult = 'You lose. Scissors cuts paper';
-        return roundResult; 
+}
+else if (userSelection === 'paper' && computerSelection === 'scissors') {
+roundResult = 'You lose. Scissors cuts paper';
+return roundResult;
 
-    }
-    else if (userSelection === 'paper' && computerSelection === 'rock') {
-        roundResult = 'You win. Paper covers rock';
-        return roundResult; 
+}
+else if (userSelection === 'paper' && computerSelection === 'rock') {
+roundResult = 'You win. Paper covers rock';
+return roundResult;
 
-    }
-    else if (userSelection === 'paper' && computerSelection === 'paper') {
-        roundResult = 'Tie';
-        return roundResult; 
+}
+else if (userSelection === 'paper' && computerSelection === 'paper') {
+roundResult = 'Tie';
+return roundResult;
 
-    }
-    else if (userSelection === 'scissors' && computerSelection === 'scissors') {
-        roundResult = 'Tie'; 
-        return roundResult; 
+}
+else if (userSelection === 'scissors' && computerSelection === 'scissors') {
+roundResult = 'Tie';
+return roundResult;
 
-    }
-    else if (userSelection === 'scissors' && computerSelection === 'paper') {
-        roundResult = 'You win. Scissors cuts paper';
-        return roundResult; 
-    }
-    else if (userSelection === 'scissors' && computerSelection === 'rock'){
-        roundResult = 'You lose. Rock beats scissors';
-    return roundResult;
+}
+else if (userSelection === 'scissors' && computerSelection === 'paper') {
+roundResult = 'You win. Scissors cuts paper';
+return roundResult;
+}
+else if (userSelection === 'scissors' && computerSelection === 'rock'){
+roundResult = 'You lose. Rock beats scissors';
+return roundResult;
 }
 */ /*
 }
+const rockButton = document.createElement('p');
+rockButton.textContent = 'rock';
+const paperButton = document.createElement('button');
+paperButton.textContent = 'paper';
+const scissorsButton = document.createElement('button');
+scissorsButton.textContent = 'scissors';
 
-
-
+document.body.appendChild(paperButton);
+    document.body.appendChild(scissorsButton);
 
 
 
@@ -102,4 +108,4 @@ function playRound () {
 
 
 /* main two problems are saying i won when my score was less than the computers and
-when the round number doesn't go up*/ 
+when the round number doesn't go up*/
